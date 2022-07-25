9d724a1c50f312bf95f4c33bedfa27f44a9e36bb
joaquinTgauna <joaquingauna14@gmail.com>
2022-07-25 19:56:17 -0300

    entrega de Dom

--- main.js
+++ main.js
@@ -20,13 +20,24 @@ while (entrada != "salir") {
 console.log ("Gracias,vuelvas prontos")
 */
 
-//segunda entrega
+//entregas varias
+
+
+
+
+function mostrarProductos(precios) {
+    document.getElementById("teclado").innerHTML = "Teclado $ " + precios.teclado
+    document.getElementById("mouse").innerHTML = "mouse $ " + precios.mouse
+    document.getElementById("monitor").innerHTML = "monitor $ " + precios.monitor
+    document.getElementById("combo").innerHTML = "combo $ " + precios.combo
+}
+
 
 let precios = {
     teclado: 600,
     mouse: 300,
     monitor: 1000,
-    combo: 1900 
+    combo: 1900
 }
 let productos = {
     teclado: "teclado",
@@ -44,117 +55,136 @@ let chango = 0;
 let valorCuota = 0;
 let cuotas = 0;
 
-
-let productoingresado = prompt('ingrese su producto ("continuar" para confirmar el medio de pago)')
-
-while (productoingresado != "continuar") {
-    switch (productoingresado) {
-        case ("teclado"):
-            chango = chango + precios.teclado
-            listachango.push(productos.teclado + "= $" + precios.teclado);
-            precioschango.push(precios.teclado);
-            break;
-        case ("mouse"):
-            chango = chango + precios.mouse
-            listachango.push(productos.mouse + "= $" + precios.mouse);
-            precioschango.push(precios.mouse);
-            break;
-
-        case ("monitor"):
-            chango = chango + precios.monitor
-            listachango.push(productos.monitor + "= $" + precios.monitor);
-            precioschango.push(precios.monitor);
-            break;
-
-        case ("combo"):
-            chango = chango + precios.combo
-            listachango.push(productos.combo + "= $" + precios.combo);
-            precioschango.push(precios.combo);
-            break;
-
-        case ("Teclado"):
-            chango = chango + precios.teclado
-            listachango.push(productos.teclado + "= $" + precios.teclado);
-            break;
-        case ("Mouse"):
-            chango = chango + precios.mouse
-            listachango.push(productos.mouse + "= $" + precios.mouse);
-            precioschango.push(precios.mouse);
-            break;
-
-        case ("Monitor"):
-            chango = chango + precios.monitor
-            listachango.push(productos.monitor + "= $" + precios.monitor);
-            precioschango.push(precios.monitor);
-            break;
-
-        case ("Combo"):
-            chango = chango + precios.combo
-            listachango.push(productos.combo + "= $" + precios.combo);
-            precioschango.push(precios.combo);
-            break;
-
-        default:
-            alert("No hay stock en este momento")
-            break;
-    }
-
-    productoingresado = prompt('Ingrese su texto ("continuar" para confirmar el medio de pago)')
-
-}
-sumaDePreciosChango(precioschango)
-
-medioDePago = prompt('ingrese efectivo o tarjeta para continuar,para finalizar su compra escriba "finalizar"')
-
-while (medioDePago != "finalizar") {
-    switch (medioDePago) {
-
-        case ("efectivo"):
-            chango = divPorDos(chango)
-            break;
-
-        case ("tarjeta"):
-            cuotas = prompt("indique cantida de cuotas")
-            valorCuota = calcularCuotas(chango, cuotas)
-            console.log(valorCuota + "resultado de las cuotas")
-            break;
+let productoingresado;
+mostrarProductos(precios)
 
 
-        default:
-            alert("medio de pago incorrecto")
-            break;
+
+setTimeout(()=>{
+    agregarProductos();
+    sumaDePreciosChango(precioschango);
+    realizarPago();
+    finalizarCompra();
+}, 500)
 
 
-    }
 
-    medioDePago = prompt('ingrese Efectivo o Tarjeta,para finalizar su compra escriba "finalizar"')
 
-}
-alert("Gracias por su compra")
-
-alert("total de la compra $" + total + ".\n Los productos que estas llevando son:\n" + listachango)
-
-if (cuotas != 0) {
-    alert("vas a pagar " + cuotas + " cuotas de $" + valorCuota)
-
-sumaDePreciosChango(precioschango)
-}
-
-function divPorDos(chango) {
-    return chango / 2
-
-}
 function calcularCuotas(chango, cuotas) {
     return chango / cuotas
 
 }
 
-function sumaDePreciosChango (precioschango){
-let sumasChango = 0;
-    precioschango.forEach( (numero) => {
-    sumasChango+=numero
-        
-});
-return total = sumasChango
+function sumaDePreciosChango(precioschango) {
+    let sumasChango = 0;
+    precioschango.forEach((numero) => {
+        sumasChango += numero
+
+    });
+    return total = sumasChango
 }
-console.log(total)
+
+function agregarProductos() {
+    productoingresado = prompt('ingrese su producto ("continuar" para confirmar el medio de pago)')
+    while (productoingresado != "continuar") {
+        switch (productoingresado) {
+            case ("teclado"):
+                chango = chango + precios.teclado
+                listachango.push(productos.teclado + "= $" + precios.teclado);
+                precioschango.push(precios.teclado);
+                break;
+            case ("mouse"):
+                chango = chango + precios.mouse
+                listachango.push(productos.mouse + "= $" + precios.mouse);
+                precioschango.push(precios.mouse);
+                break;
+
+            case ("monitor"):
+                chango = chango + precios.monitor
+                listachango.push(productos.monitor + "= $" + precios.monitor);
+                precioschango.push(precios.monitor);
+                break;
+
+            case ("combo"):
+                chango = chango + precios.combo
+                listachango.push(productos.combo + "= $" + precios.combo);
+                precioschango.push(precios.combo);
+                break;
+
+            case ("Teclado"):
+                chango = chango + precios.teclado
+                listachango.push(productos.teclado + "= $" + precios.teclado);
+                break;
+            case ("Mouse"):
+                chango = chango + precios.mouse
+                listachango.push(productos.mouse + "= $" + precios.mouse);
+                precioschango.push(precios.mouse);
+                break;
+
+            case ("Monitor"):
+                chango = chango + precios.monitor
+                listachango.push(productos.monitor + "= $" + precios.monitor);
+                precioschango.push(precios.monitor);
+                break;
+
+            case ("Combo"):
+                chango = chango + precios.combo
+                listachango.push(productos.combo + "= $" + precios.combo);
+                precioschango.push(precios.combo);
+                break;
+
+            default:
+                alert("No hay stock en este momento")
+                break;
+        }
+
+        productoingresado = prompt('Ingrese su texto ("continuar" para confirmar el medio de pago)')
+
+    }
+}
+
+function realizarPago() {
+    medioDePago = prompt('ingrese efectivo o tarjeta para continuar,para finalizar su compra escriba "finalizar"')
+
+    while (medioDePago != "finalizar") {
+        switch (medioDePago) {
+
+            case ("efectivo"):
+                chango = chango / 2
+                break;
+
+            case ("tarjeta"):
+                cuotas = prompt("indique cantida de cuotas")
+                valorCuota = calcularCuotas(chango, cuotas)
+                console.log(valorCuota + "resultado de las cuotas")
+                break;
+
+
+            default:
+                alert("medio de pago incorrecto")
+                break;
+
+
+        }
+
+        medioDePago = prompt('ingrese Efectivo o Tarjeta,para finalizar su compra escriba "finalizar"')
+
+    }
+
+
+
+}
+
+function finalizarCompra() {
+
+    alert("Gracias por su compra")
+
+    alert("total de la compra $" + total + ".\n Los productos que estas llevando son:\n" + listachango)
+
+    if (cuotas != 0) {
+        alert("vas a pagar " + cuotas + " cuotas de $" + valorCuota)
+
+        sumaDePreciosChango(precioschango)
+    }
+
+}
